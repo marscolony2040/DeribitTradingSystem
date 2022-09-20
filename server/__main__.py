7a541f65d847b6c0005a7be2c8f02bc83f11da58
MoSha256 <54046329+marscolony2040@users.noreply.github.com>
2022-09-20 18:53:36 -0400

    Update __main__.py

--- server/__main__.py
+++ server/__main__.py
@@ -19,45 +19,8 @@ async def start(ws, path):
                 'ticker.BTC-PERPETUAL.raw']
     await System(key, secret, channels).start(ws)
 
-# Decorator handles system error and reboots
-def refresher(f):
-    def handle(*a, **b):
-        while True:
-            try:
-                print('RAZR Trading System')
-                f(*a, **b)
-            except Exception as e:
-                print('System Error, Rebooting: {}'.format(e))
-            time.sleep(3)
-    return handle
 
-# Decorator creates thread
-def multi(f):
-    def threader(*a, **b):
-        return threading.Thread(target=lambda: f(*a, **b))
-    return threader
+loop = asyncio.get_event_loop()
+loop.run_until_complete(websockets.serve(start, host, port))
+loop.run_forever()
 
-# Opens server connection
-@multi
-def server():
-    print('Starting Python Server')
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(websockets.serve(start, host, port))
-    loop.run_forever()
-
-# Opens React.js client
-@multi
-def client():
-    print('Starting React FrontEnd')
-    time.sleep(2.67)
-    call(["npm", "start"])
-
-# Main function opens and starts the client/server threads
-@refresher
-def main(s, c):
-    for y in (s, c): y.start()
-    for y in (s, c): y.join()
-
-# Trading system opener
-main(server(), client())
